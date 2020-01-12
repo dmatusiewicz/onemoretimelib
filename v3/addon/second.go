fa0d5195cefea9c6acf83c09d5b612a25e867fdf
Dawid M <dawidm>
2020-01-12 18:55:14 +0100

    Publish v3.2.0

--- v3/addon/second.go
+++ v3/addon/second.go
@@ -3,5 +3,5 @@ package addon
 import "fmt"
 
 func HelloAdd() {
-	fmt.Println("Hello from addon v3.0.0 XXX")
+	fmt.Println("Hello from addon v3.2.0 XXX")
 }
--- v3/first.go
+++ v3/first.go
@@ -3,5 +3,5 @@ package onemoretimelib
 import "fmt"
 
 func Hello() {
-	fmt.Println("Hello from root")
+	fmt.Println("Hello from root v3.2.0")
 }
